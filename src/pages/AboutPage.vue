23461df653eeca9d7caed4acad031d25551c64df
Matthew McPeak <m.mcpeak98@icloud.com>
2025-10-28 08:49:51 -0400

    fix(About): Fixed an issue with the image

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -27,7 +27,7 @@ onBeforeMount(async () => {
       <div
         class="flex flex-row grow justify-center rounded-lg border-2 shadow-xl transition duration-200 ease-in-out hover:border-emerald-700 basis-1/3 border-zinc-800"
       >
-        <img class="rounded-lg grayscale" :src="user.avatar_url" />
+        <img class="rounded-lg grayscale grow" :src="user.avatar_url" />
       </div>
       <div class="flex flex-col justify-center p-5 basis-full lg:basis-2/3">
         <p class="text-3xl transition duration-200 ease-in-out hover:text-emerald-500 fancy-text">
