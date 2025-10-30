9eaaa761cd8538487c21136c01a5bd3241e5cfc2
Matthew McPeak <m.mcpeak98@icloud.com>
2025-10-30 10:03:11 -0400

    fix(formatting): Fixed formatting for the Github Project Component

--- src/components/GithubProject.vue
+++ src/components/GithubProject.vue
@@ -13,13 +13,13 @@ const { openLink } = useUtils()
     @click="openLink(html_url)"
     class="p-2 my-2 mx-4 rounded-lg border shadow-xl transition duration-200 ease-in-out cursor-pointer hover:border-emerald-700 border-zinc-800 bg-zinc-700 basis-1/2"
   >
-    <p class="transition duration-200 ease-in-out hover:text-emerald-500">"Name: "{{ name }}</p>
+    <p class="transition duration-200 ease-in-out hover:text-emerald-500">Name: {{ name }}</p>
     <p class="transition duration-200 ease-in-out hover:text-emerald-500">
-      "Language: " {{ language }}
+      Language: {{ language }}
     </p>
 
     <p class="transition duration-200 ease-in-out hover:text-emerald-500">
-      "Publish Date: " {{ new Date(created_at).toLocaleDateString('en-US') }}
+      Publish Date: {{ new Date(created_at).toLocaleDateString('en-US') }}
     </p>
     <p
       class="overflow-y-auto wrap-break-word transition duration-200 ease-in-out hover:text-emerald-500 h-fit text-wrap"
